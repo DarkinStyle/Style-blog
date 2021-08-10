02f32b85a736efd8fb44532602940e095d9b3e12
DarkinStyle <leewaihin2614@gmail.com>
2021-08-10 19:40:21 +0800

    some changes

--- src/App.js
+++ src/App.js
@@ -10,16 +10,7 @@ function App() {
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        {/* <p>{ person }</p> */}
-
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas "}</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-        <a href={ link }>Google Site</a>
-        <p>hi</p>
+       
       </div>
     </div>
   );
