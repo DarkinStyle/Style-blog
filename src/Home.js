74fe3412752b3e4824d657c912035ad1c06c61be
DarkinStyle <leewaihin2614@gmail.com>
2021-08-10 23:13:23 +0800

    done with loading message

--- src/Home.js
+++ src/Home.js
@@ -7,22 +7,25 @@ const Home = () => {
     const [blogs, setBlogs] = useState(null);
 
     const [name, setName] = useState('mario');
-
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs') // fetching it from resources as a promise
+        setTimeout(() => { // just to demostrate the loading message by set a timer
+            fetch('http://localhost:8000/blogs') // fetching it from resources as a promise
             .then(res => {
                 return res.json();
             })
             .then((data) => {
                 setBlogs(data);
+                setIsPending(false);
             })
+        }, 1000);
     }, []); 
 
     return (  
         <div className="home">
+            { isPending && <div>Loading....</div>}
             { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
-            
         </div>
     );
 }
