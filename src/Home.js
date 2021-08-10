e731a4fe900e10f2ae51f7212251f0eb80ac026a
DarkinStyle <leewaihin2614@gmail.com>
2021-08-10 20:15:13 +0800

    for click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello, everyone', e); // e is the event object
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('mario', e)}>click me again</button>
         </div>
     );
 }
