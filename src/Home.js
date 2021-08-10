389dfa275cd81e6cd57a6ef521e45de34ebbcf2b
DarkinStyle <leewaihin2614@gmail.com>
2021-08-10 20:30:33 +0800

    update on usestate

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log('Hello, everyone', e); // e is the event object
+    const [name, setName] = useState('style');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+       setName('zap');
+       setAge(50);
     }
-    
+
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('mario', e)}>click me again</button>
+            
         </div>
     );
 }
