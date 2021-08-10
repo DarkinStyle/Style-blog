ac2db0c956224eea8bb48a40a5f92cfc910cf846
DarkinStyle <leewaihin2614@gmail.com>
2021-08-10 19:50:59 +0800

    cleaning

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import Home from './Home';
 
 function App() {
 
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = { name: 'yoshi', age: 30 };
-  const link = "http://www.google.com";
-
   return (
     <div className="App">
       <Navbar />
