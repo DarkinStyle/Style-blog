4c05df9e5bf4f0399399532b0a6ae7554bc0c06d
DarkinStyle <leewaihin2614@gmail.com>
2021-08-10 23:23:37 +0800

    done fetch errors

--- src/Home.js
+++ src/Home.js
@@ -8,22 +8,33 @@ const Home = () => {
 
     const [name, setName] = useState('mario');
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => { // just to demostrate the loading message by set a timer
             fetch('http://localhost:8000/blogs') // fetching it from resources as a promise
             .then(res => {
+                
+                if(!res.ok) {
+                    throw Error('could not fetch the data for that resource');
+                }
                 return res.json();
             })
             .then((data) => {
                 setBlogs(data);
                 setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
             })
         }, 1000);
     }, []); 
 
     return (  
         <div className="home">
+            { error && <div>{ error }</div> }
             { isPending && <div>Loading....</div>}
             { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
         </div>
