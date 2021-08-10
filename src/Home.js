837346ce3c18c31575f512fdc7429341594bd998
DarkinStyle <leewaihin2614@gmail.com>
2021-08-10 21:49:06 +0800

    finish version

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { readBuilderProgram } from 'typescript';
+import BlogList from './BlogList';
 
 const Home = () => {
 
@@ -9,16 +10,12 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    // props for bloglist: blogs={blogs}
+
     return (  
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
-            
-            
+            <BlogList blogs={blogs} title="All Blogs"/> 
+        
         </div>
     );
 }
